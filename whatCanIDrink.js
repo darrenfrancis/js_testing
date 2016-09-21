5618471a47273f318337bf3f5df05d0d6c7800e9
Darren <dazfrancisllpg@hotmail.com>
2016-09-21 19:40:31 +0100

    Adding console log

--- whatCanIDrink.js
+++ whatCanIDrink.js
@@ -1,12 +1,14 @@
 function whatCanIDrink(age) {
-	if (age <= 0;) {
+	if (age <= 0) {
 		return "Sorry. I can't tell what drink because that age is incorrect!"
-	} else if (age <= 14;) { return "Drink Toddy!"
-	} else if (age <= 18;) { return "drink coke"
-	} else if (age <= 21;) { return "drink Beer"
-	} else if (age <= 130;) { return "drink whiskey"
+	} else if (age <= 14) { return "Drink Toddy!"
+	} else if (age <= 18) { return "drink coke"
+	} else if (age <= 21) { return "drink Beer"
+	} else if (age <= 130) { return "drink whiskey"
 	} else { return "Sorry. I can't tell what drink because that age is incorrect!"}
 
 
 	
 }
+
+console.log(whatCanIDrink(30);
