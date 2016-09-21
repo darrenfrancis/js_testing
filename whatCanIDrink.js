87d34d0977cd1d8746dd1696cb1e8ec87ad06f9b
Darren <dazfrancisllpg@hotmail.com>
2016-09-21 19:45:09 +0100

    Additional age ranges and drinks

--- whatCanIDrink.js
+++ whatCanIDrink.js
@@ -4,6 +4,8 @@ function whatCanIDrink(age) {
 	} else if (age <= 14) { return "Drink Toddy!"
 	} else if (age <= 18) { return "drink coke"
 	} else if (age <= 21) { return "drink Beer"
+	} else if (age <= 30) { return "drink jagerbombs"
+	} else if (age <= 60) { return "drink real ale"
 	} else if (age <= 130) { return "drink whiskey"
 	} else { return "Sorry. I can't tell what drink because that age is incorrect!"}
 
