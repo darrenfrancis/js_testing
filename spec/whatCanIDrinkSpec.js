1daa259cb9e7e65be911f053390f164f74911ae9
Darren <dazfrancisllpg@hotmail.com>
2016-09-23 19:31:46 +0100

    Additional tests added

--- spec/whatCanIDrinkSpec.js
+++ spec/whatCanIDrinkSpec.js
@@ -9,9 +9,16 @@ describe ("whatCanIDrink", function() {
 		});
 
 		it('should pass a 13 as an argument and return Drink coke', function() {
-			//whatCanIDrink(17); This wasn't needed
 			expect(whatCanIDrink(13)).toBe("Drink Toddy!");
 			
+		});
+
+		it ('should pass 20 as an argument and return Drink coke', function() {
+			expect(whatCanIDrink(20)).toBe("drink beer");
+		});
+
+		it("should pass some text as an argument and return 'Sorry I didnt recognize...'", function(){
+			expect(whatCanIDrink(wally)).toBe("Sorry!!. I can't tell what drink because that age is incorrect!");
 		});
 	});
 });
